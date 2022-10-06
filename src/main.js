9c09a795d7ed689214cec12b209d6c143c029404
Cesar <camongez28@gmail.com>
2022-10-05 21:30:52 -0300

    git

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import app from './server.js';
 
 const PORT = 8080;
 const server = app.listen(PORT, () => {
-  console.log(
-    `Servidor http .escuchando en el puerto ${server.address().port}`
-  );
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
 });
 server.on('error', (error) => console.log(`Error en servidor ${error}`));
